ae95b7a471cc815bc95d59b462a552da247256be
stanley355 <winatastanley355@gmail.com>
2022-01-22 09:42:39 +0700

    fault hasher to number

--- src/shorten.rs
+++ src/shorten.rs
@@ -1,12 +1,25 @@
-use actix_web::{ web, Result };
-use serde::Deserialize;
+use actix_web::{web, Responder, Result};
+use serde::{Deserialize, Serialize};
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
 
-#[derive(Deserialize)]
-pub struct Info {
-    username: String,
+#[derive(Deserialize, Serialize, Debug)]
+pub struct RequestURL {
+  origin_url: String,
+}
+
+#[derive(Serialize, Debug)]
+pub struct ResponseURL {
+  origin_url: String,
 }
 
 /// extract `Info` using serde
-pub async fn shorten_url(info: web::Json<Info>) -> Result<String> {
-  Ok(format!("Welcome {}!", info.username))
+pub async fn shorten_url(req: web::Json<RequestURL>) -> Result<impl Responder> {
+  let mut hasher = DefaultHasher::new();
+  req.origin_url.hash(&mut hasher);
+  
+  let res = ResponseURL {
+    origin_url: hasher.finish().to_string(),
+  };
+  Ok(web::Json(res))
 }
