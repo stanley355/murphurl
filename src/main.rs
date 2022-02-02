43d942472511f2118c48f84f6103a52ebf4a81e1
stanley355 <winatastanley355@gmail.com>
2022-02-02 14:17:41 +0700

    test

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ async fn main() -> std::io::Result<()> {
             .route("/v1/{url}", web::get().to(shorten::find_shorten_url))
             .route("/v1", web::post().to(shorten::shorten_url))
     })
-    .bind(&env::var("HOST").unwrap())?
+    .bind(&env::var("PORT").unwrap())?
     .run()
     .await
 }
