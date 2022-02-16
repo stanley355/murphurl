774cdb3b957538a5983516b3fd55d925228a7bf2
stanley355 <winatastanley355@gmail.com>
2022-02-16 09:07:05 +0700

    initiate client on main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{web, App, HttpServer};
+use sqlx::postgres::PgPool;
 use dotenv::dotenv;
 use std::env;
 
@@ -8,19 +9,21 @@ mod shorten;
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
 
-    let address = format!(
-        "{}:{}",
-        &env::var("HOST").unwrap_or("127.0.0.1".to_string()),
-        &env::var("PORT").unwrap_or("8080".to_string())
-    );
+    let pg_url = &env::var("PG_URL").unwrap();
+    let host = &env::var("HOST").unwrap_or("127.0.0.1".to_string());
+    let port = &env::var("PORT").unwrap_or("8080".to_string());
+    let address = format!("{}:{}", host, port);
 
-    // to do: use app scope
-    HttpServer::new(|| {
-        App::new().service(
-            web::scope("/api")
-                .route("/v1", web::post().to(shorten::shorten_url)) //to create shorten URL
-                .route("/v1/{url}", web::get().to(shorten::find_redirect_url)), //to find origin URL
-        )
+    let pg_pool = match PgPool::connect(pg_url).await {
+        Ok(pool) => Box::new(pool),
+        Err(err) => panic!("Can't connect to PG POOL: {}", err),
+    };
+
+    HttpServer::new(move || {
+        App::new()
+        .app_data(pg_pool.clone())
+        .route("/v1", web::post().to(shorten::shorten_url)) //to create shorten URL
+        .route("/v1/{url}", web::get().to(shorten::find_redirect_url)) //to find origin URL  
     })
     .bind(address)?
     .run()
