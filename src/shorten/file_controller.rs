53ff8d5e3199668e2a6cda851d5bc1719d76daea
stanley355 <winatastanley355@gmail.com>
2022-02-20 14:23:34 +0700

    revis file upload

--- src/shorten/file_controller.rs
+++ src/shorten/file_controller.rs
@@ -5,25 +5,21 @@ use actix_multipart::Multipart;
 use actix_web::web;
 
 pub async fn save_file(mut payload: Multipart) -> Option<bool> {
-  // iterate over multipart stream
-  while let Ok(Some(mut field)) = payload.try_next().await {
-    let content_type = field.content_disposition().unwrap();
-    let filepath = format!("uploads/{}", content_type.get_filename()?);
+  let mut field = payload.try_next().await.unwrap()?;
+  let content_type = field.content_disposition().unwrap();
+  let filename = format!("uploads/{}", content_type.get_filename()?);
 
-    // File::create is blocking operation, use threadpool
-    let mut file = web::block(|| std::fs::File::create(filepath))
-      .await
-      .unwrap();
+  // File::create is blocking operation, use threadpool
+  let mut file = web::block(|| std::fs::File::create(filename))
+    .await
+    .unwrap();
 
-    // Field in turn is stream of *Bytes* object
-    while let Some(chunk) = field.next().await {
-      let data = chunk.unwrap();
-      // filesystem operations are blocking, we have to use threadpool
-      file = web::block(move || file.write_all(&data).map(|_| file))
-        .await
-        .unwrap();
-    }
-  }
+  // Field in turn is stream of *Bytes* object
+  let data_chunk = field.next().await.unwrap().unwrap();
+  // filesystem operations are blocking, we have to use threadpool
+  web::block(move || file.write_all(&data_chunk).map(|_| file))
+    .await
+    .unwrap();
 
   Some(true)
 }
