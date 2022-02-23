2dc2e42d4790fd63579778d392c36d1ce87b8210
stanley355 <winatastanley355@gmail.com>
2022-02-23 09:46:46 +0700

    Success in reading excel but fail in writing it

--- src/shorten/file_controller.rs
+++ src/shorten/file_controller.rs
@@ -1,13 +1,18 @@
 use futures::{StreamExt, TryStreamExt};
+use office::{DataType, Excel};
 use std::io::Write;
 
 use actix_multipart::Multipart;
 use actix_web::web;
 
+use crate::shorten::model::ShortURL;
+use crate::shorten::utils::hash_url;
+
 pub async fn save_file(mut payload: Multipart) -> Option<bool> {
   let mut field = payload.try_next().await.unwrap()?;
   let content_type = field.content_disposition()?;
   let filename = format!("uploads/{}", content_type.get_filename()?);
+  let a = filename.clone();
 
   // Create filename without inserting the data
   // File::create is blocking operation, use threadpool
@@ -23,5 +28,38 @@ pub async fn save_file(mut payload: Multipart) -> Option<bool> {
     .await
     .expect("Fail to insert file data");
 
+  read_file(a);
+
   Some(true)
+}
+
+pub fn read_file(path: String) {
+  // opens a new workbook
+  let mut excel = Excel::open(path).expect("Fail to find workbook");
+  let mut sheet = excel.worksheet_range("Sheet1").unwrap();
+
+  sheet.set_value((1, 1), office::DataType::String("wkakwkw".to_string()));
+  let mut count = 1;
+  let row_len = sheet.rows().count();
+
+  while count < row_len  {
+    let main_url = match sheet.get_value(count, 0).clone() {
+      DataType::String(value) => value,
+      _ => "".to_string(),
+    };
+    let customized = match sheet.get_value(count, 2).clone() {
+      DataType::String(value) => value,
+      _ => "".to_string(),
+    };
+    let short_url = ShortURL {
+      origin_url: main_url.clone(),
+      hashed_url: hash_url(&main_url),
+      custom_url: customized,
+    };
+
+    let processed_url = short_url.verify_and_hash();
+    sheet.set_value((count as u32, 1), office::DataType::String(processed_url.unwrap().hashed_url));
+
+    count += 1;
+  }
 }
